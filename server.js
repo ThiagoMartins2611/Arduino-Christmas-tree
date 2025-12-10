ab55e37efa4be5bcb2ffdfcdf06b60daa744cf36
Thiago Costa Martins <thiagogames2611@gmail.com>
2025-12-10 01:48:20 -0300

    Adcionado o servidor e codigo

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+app = express()
+routes = require('express').Router()
+
+
+routes.get('/arduino/:sensor', (req, res) =>{
+
+    const valor = req.params.sensor
+    if(!valor){
+        return res.status(400).send({error: 'Parametro ou outro erro no ESP'})
+    }
+
+    console.log('valor recebido', valor)
+    res.status(200).send({valor});
+})
+
+app.use('/', routes)
+
+
+
+app.listen(3000, ()=>{
+console.log('Aplica¸c~ao rodando!')
+})
