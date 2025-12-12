09515bf836bbfba456071889ffbad4ddf7b149af
ThiagoMartins2611 <thiagogames2611@gmail.com>
2025-12-12 10:37:32 -0300

    Alterações de visualização para mostrar somente a luminosidade

--- server.js
+++ server.js
@@ -1,23 +1,43 @@
-const express = require('express')
-app = express()
-routes = require('express').Router()
+const express = require('express');
+const app = express();
+const routes = require('express').Router();
 
 
-routes.get('/teste/:sensor', (req, res) =>{
+app.use(express.static("./public"));
 
-    const valor = req.params.sensor
-    if(!valor){
-        return res.status(400).send({error: 'Parametro ou outro erro no ESP'})
+
+global.valor = 0;
+
+
+routes.get('/teste/:sensor', (req, res) => {
+    const valor = req.params.sensor;
+    if (!valor) {
+        return res.status(400).send({ error: 'Parametro invalido' });
     }
 
-    console.log('valor recebido', valor)
-    res.status(200).send({valor});
+    console.log(valor)
+
+    global.valor = valor;
+    res.status(200).send({ valor });
+});
+
+
+routes.get('/dados', (req, res) => {
+    try {
+        console.log("Valor global:", global.valor)
+        res.status(200).send({ luminosidade: global.valor });
+    } catch (error) {
+        console.log(error)
+    }
+  
+});
+
+routes.get("/", (req, res)=>{
+    res.sendFile("pag.html", {root: "./public"});
 })
 
-app.use('/', routes)
+app.use('/', routes);
 
-
-
-app.listen(3000, ()=>{
-console.log('Aplica¸c~ao rodando!')
-})
+app.listen(3000, () => {
+    console.log('Aplicação rodando!');
+});
